b61f4febd17cc26471c76727ec1fa732aaeff81c
22714869@qq.com <800223>
2018-04-21 15:34:04 +0800

    int

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
-const devEnv = require('./dev.env')
+const devEnv = require('./prod.env')
 
 module.exports = {
   dev: {
@@ -14,7 +14,7 @@ module.exports = {
     // 代理列表, 是否开启代理通过[./dev.env.js]配置
     proxyTable: devEnv.OPEN_PROXY === false ? {} : {
       '/proxyApi': {
-        target: 'http://demo.renren.io/renren-fast/',
+        target: 'http://127.0.0.1:8080/renren-fast/',
         changeOrigin: true,
         pathRewrite: {
           '^/proxyApi': '/'
